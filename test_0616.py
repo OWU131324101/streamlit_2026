d8f3a51ed3d903fe191656822c90f1b74b9450cd
OWU131324101 <s1324101@cst.otsuma.ac.jp>
2026-06-16 09:20:01 +0900

    Create test_0616.py

---
+++ test_0616.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from datetime import datetime, time
+
+st.header("アルバイトシフト管理システム")
+
+if "shifts" not in st.session_state:
+    st.session_state.shifts = []
+
+name = st.text_input(
+    label = "アルバイト先を入力してください",
+    value = "",
+    help="あなたが働いているアルバイト先の名前を入力してください",
+    placeholder="例: すたば"
+)
+
+with st.form("cafe_order"):
+    date = st.date_input(
+        label = "日付を入力してください",
+        value = datetime.now(),
+        help = "カレンダーから日付を選択"
+    )
+
+    time1 = st.time_input(
+        label = "開始時刻",
+        value = time(12,0),
+        help = "アルバイト開始時刻を選択"
+    )
+
+    time2 = st.time_input(
+        label = "終了時刻",
+        value = time(12,0),
+        help = "アルバイト終了時刻を選択"
+    )
+
+
+    submitted = st.form_submit_button("登録する")
+    if submitted:
+        st.session_state.shifts.append({
+            "日付": date,
+            "開始": time1,
+            "終了": time2
+        })
+
+        st.success("シフトを登録しました")
+
+
+
+if name:
+    st.subheader(f"{name}の予定表")
+
+for shift in st.session_state.shifts:
+    st.write(
+        f"{shift['日付']} "
+        f"{shift['開始']} ～ {shift['終了']}"
+    )
